93b2cfd89342985bf362d10f924d9a25dfe6ba53
一个木的感情的小卷卷 <reginvolver@gmail.com>
2024-02-21 16:24:53 +0800

    cors

---
+++ src/main/java/com/ydsy/javademo/filter/CorsFilter.java
@@ -0,0 +1,39 @@
+package com.ydsy.javademo.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+
+        // 允许来自所有域的跨域请求
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        // 允许的请求方法
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        // 允许的请求头部信息
+        response.setHeader("Access-Control-Allow-Headers", "*");
+
+        System.out.println("CORS filter执行了！");
+        filterChain.doFilter(request, response);
+    }
+
+    // 可选的初始化和销毁方法
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // 初始化代码
+    }
+
+    @Override
+    public void destroy() {
+        // 销毁代码
+    }
+}
